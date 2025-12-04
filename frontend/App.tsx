693491643c071e2d4c81e726dd32aeb580107a8a
yukselaciker <yukselaciker@gmail.com>
2025-12-05 00:37:07 +0300

    Otomatik güncelleme

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -20,7 +20,7 @@ import About from './components/About';
 import Services from './components/Services';
 import Features from './components/Features';
 import Testimonials from './components/Testimonials';
-import MathAssistant from './components/MathAssistant';
+
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import BookingModal from './components/BookingModal';
@@ -73,7 +73,7 @@ const AppContent: React.FC = () => {
         <Features />
         <Testimonials />
         <FAQ />
-        <MathAssistant />
+
         <Contact />
       </main>
 
